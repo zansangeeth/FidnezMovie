b2c6a8590d18df3bcd945a93b41dc4b5b166de36
zansangeeth <zansangeeth17@gmail.com>
2022-11-25 11:30:33 +0530

    updated bottom nav bar with fragments

--- app/src/main/java/com/zasa/fidnezmovie/MainActivity.kt
+++ app/src/main/java/com/zasa/fidnezmovie/MainActivity.kt
@@ -2,12 +2,40 @@ package com.zasa.fidnezmovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.zasa.fidnezmovie.databinding.ActivityMainBinding
 
 // api key - 90ae9a90aaa4867e2249e54752d1a259
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(PopularFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.popular -> replaceFragment(PopularFragment())
+                R.id.upcoming -> replaceFragment(UpcommingFragment())
+                else -> {
+                }
+            }
+            true
+        }
+    }
+
+    // replace fragment layout
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
