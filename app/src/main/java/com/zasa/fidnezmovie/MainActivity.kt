73d11cbf784b8fa69fc5f625873e51341388da73
zansangeeth <zansangeeth17@gmail.com>
2022-11-25 11:13:24 +0530

    first commit with skeleton of the app

---
+++ app/src/main/java/com/zasa/fidnezmovie/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.zasa.fidnezmovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// api key - 90ae9a90aaa4867e2249e54752d1a259
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/zasa/fidnezmovie/UpcommingFragment.kt
@@ -0,0 +1,19 @@
+package com.zasa.fidnezmovie
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class UpcommingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_upcomming, container, false)
+    }
+
+}
